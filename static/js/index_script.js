c91de70ad48b3b2db90e792df449c10370bb8362
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2024-04-15 18:47:37 +0800

    日夜模式切换判断

--- static/js/index_script.js
+++ static/js/index_script.js
@@ -12,3 +12,21 @@ setInterval(function () {
     
     next.setAttribute('data-show', '')
   }, 2000)
+
+function toggleIcon() {
+    var icon = document.querySelector('link[rel~="icon"]');
+    if (matchMedia('(prefers-color-scheme: dark)').matches) {
+        icon.href = '/static/img/icon.svg'; // 白天模式图标
+    } else {
+        icon.href = '/static/img/icon_dark.svg'; // 夜间模式图标
+    }
+    var logo = document.querySelector('img[class~="logo"]');
+    if (matchMedia('(prefers-color-scheme: dark)').matches) {
+        logo.href = '/static/img/icon.svg';
+    } else {
+        logo.href = '/static/img/icon_dark.svg';
+    }
+}
+ 
+window.addEventListener('load', toggleIcon);
+matchMedia('(prefers-color-scheme)').addEventListener('change', toggleIcon);
